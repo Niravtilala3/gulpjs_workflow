af73c8b42edd2f6fb277525d06c80048a14fb003
Niravtilala <niravtilala3@gmail.com>
2018-09-25 09:02:59 +0530

    add css task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
-var gulp = require('gulp'),
-	gutil = require('gulp-util');
 
+var gulp = require('gulp'),
+    gutil = require('gulp-util')
+    minifyCSS = require('gulp-csso'),
+	concat = require('gulp-concat'),
+	del = require('del');
+
+// say hi using gulp
 gulp.task('hi',function(){
 	gutil.log("Hi");
 });
-gulp.task('default', [ 'hi' ]);
+
+
+// minfide and concat css files
+gulp.task('css',function(){
+	return gulp.src('./css/*.css')
+		.pipe(minifyCSS())
+		.pipe(concat('main.min.css'))
+		.pipe(gulp.dest('build/css'))
+});
+
+
+//delete build folder.
+gulp.task('clean', function(){ 
+	del(['build'])
+});
+
+
+gulp.task('default', [ 'hi','css']);
